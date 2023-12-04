87fc3d0e6907dd404e3d78e5968f8eb19c5a8487
iqattami <iqattami@c4r4p1.1337.ma>
2023-12-04 17:33:06 +0100

    update

---
+++ ft_putnbrB.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbrB.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iqattami <iqattami@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 17:32:15 by iqattami          #+#    #+#             */
+/*   Updated: 2023/12/04 17:32:17 by iqattami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <unistd.h>
+
+int ft_putnbrB(unsigned long int n, int base, const char *symbol)
+{
+    int nbr;
+    if
+    nbr = 0;
+    if(n < 16)
+    {
+        nbr += ft_putchar(symbol[n]);
+    }
+    else
+    {
+        nbr += ft_putnbrB((n / base),base, symbol);
+        nbr += ft_putnbrB((n % base), base, symbol);
+    }
+    return (nbr);
+} 
